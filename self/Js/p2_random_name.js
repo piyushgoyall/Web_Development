058ce077c29d27f446bbfc1bffd827f307eb53fd
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-08-16 23:44:41 +0530

    update

--- self/Js/p2_random_name.js
+++ self/Js/p2_random_name.js
@@ -17,6 +17,8 @@ Hub
 
 */
 
+/*
+
 let adj = {
   a: "Crazy",
   b: "Amazing",
@@ -45,3 +47,39 @@ const keys3 = Object.keys(shop);
 const randomKey3 = keys3[Math.floor(Math.random() * keys3.length)];
 
 console.log(adj[randomKey1] + " " + shop[randomKey2] + " " + word[randomKey3]);
+
+*/
+
+let rand = Math.random();
+let first, second, third;
+
+// Generate the first word
+if (rand < 0.33) {
+  first = "Crazy";
+} else if (rand < 0.66 && rand >= 0.33) {
+  first = "Amazing";
+} else {
+  first = "Fire";
+}
+
+// Generate the second word
+rand = Math.random();
+if (rand < 0.33) {
+  second = "Engine";
+} else if (rand < 0.66 && rand >= 0.33) {
+  second = "Foods";
+} else {
+  second = "Garments";
+}
+
+// Generate the third word
+rand = Math.random();
+if (rand < 0.33) {
+  third = "Bros";
+} else if (rand < 0.66 && rand >= 0.33) {
+  third = "Limited";
+} else {
+  third = "Hub";
+}
+
+console.log(`${first} ${second} ${third}`);
