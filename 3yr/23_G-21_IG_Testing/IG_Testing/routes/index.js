c9bd8a084ba586c33048a7e62c1acbecfc426ed4
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-10-24 23:58:23 +0530

    new

--- 3yr/23_G-21_IG_Testing/IG_Testing/routes/index.js
+++ 3yr/23_G-21_IG_Testing/IG_Testing/routes/index.js
@@ -5,6 +5,7 @@ const postModel = require("./post");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const upload = require("./multer");
+const uploadProfile = require("./multer2");
 
 passport.use(new localStrategy(userModel.authenticate()));
 
@@ -28,6 +29,23 @@ router.get("/upload", isLoggedIn, (req, res) => {
   res.render("upload");
 });
 
+router.get("/editprofile", isLoggedIn, (req, res) => {
+  res.render("editprofile", { user: req.user });
+});
+
+router.get("/profile", isLoggedIn, async (req, res) => {
+  const username = req.session.passport.user;
+  const user = await userModel.findOne({ username }).populate("posts");
+
+  res.render("profile", {
+    user: user,
+    username: user.username,
+    bio: user.bio,
+    profileImage: user.profileImage,
+    posts: user.posts,
+  });
+});
+
 router.post("/upload", isLoggedIn, upload.single("image"), async (req, res) => {
   const user = await userModel.findOne({ username: req.session.passport.user });
   const post = await postModel.create({
@@ -40,6 +58,49 @@ router.post("/upload", isLoggedIn, upload.single("image"), async (req, res) => {
   await user.save();
 
   res.redirect("/feed");
+});
+
+router.post(
+  "/editProfile",
+  uploadProfile.single("image"),
+  isLoggedIn,
+  async (req, res) => {
+    const user = await userModel.findOne({
+      username: req.session.passport.user,
+    });
+
+    user.name = req.body.name;
+    user.bio = req.body.bio;
+
+    if (req.file) {
+      user.profileImage = req.file.filename;
+    }
+
+    await user.save();
+    res.redirect("/profile");
+  }
+);
+
+router.post("/likePost/:postId", isLoggedIn, async (req, res) => {
+  const user = await userModel.findOne({ username: req.session.passport.user });
+  const post = await postModel.findById(req.params.postId);
+
+  const hasLiked = post.likes.includes(user._id);
+
+  if (hasLiked) {
+    // Unlike the post
+    post.likes.pull(user._id);
+    user.likes.pull(post._id);
+  } else {
+    // Like the post
+    post.likes.push(user._id);
+    user.likes.push(post._id);
+  }
+
+  await post.save();
+  await user.save();
+
+  res.json({ success: true, liked: !hasLiked, likeCount: post.likes.length });
 });
 
 /* GET home page. */
